7682ecd799851a0f16d6e0ccba4a8a815b2f4799
Htoo Maung Thait <htoomaungthait@gmail.com>
2020-01-26 01:03:06 +0630

    brocast test complete

--- app/Http/Controllers/CardController.php
+++ app/Http/Controllers/CardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Card;
+use App\Events\ScoreUpdate;
 
 class CardController extends Controller
 {
@@ -10,6 +12,8 @@ class CardController extends Controller
         $user = auth()->user();
         $user->score = $user->score + $card->value;
         $user->save();
+
+        event(new ScoreUpdate($user)); // broadcast `ScoreUpdated` event
 
         return redirect()->back()->withValue($card->value);
     }
